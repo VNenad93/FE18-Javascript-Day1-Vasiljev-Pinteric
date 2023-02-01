83ba93551130f71fa0198e3ad18373f7080463bf
vNenad <vasiljevnenad1@gmail.com>
2023-02-01 11:32:51 +0100

    inter finished

---
+++ Inter/js.js
@@ -0,0 +1,33 @@
+// Exercise #1
+
+let cars = ['Tesla', 'Audi', 'Renault', 'Volvo', 'Mazda', 'Fiar', 'Ferrari']
+
+let sortedCars = cars.sort()
+
+
+
+// Exercise #2
+
+let fruits = ['apple', 'banana', 'kiwi']
+
+fruits.push('orange')
+
+fruits.pop()
+
+// part 2
+
+let animals = ['monkey', 'horse', 'dog']
+
+animals.reverse()
+
+animals.unshift('cat')
+
+
+
+// Exercise #3
+
+let newLine = 'mango/cherries/kiwi/grapes/pear/peach/orange/lemon'
+
+let splited = newLine.split('/')
+
+document.write(`${splited[0]} \n ${splited[1]} \n ${splited[2]} \n ${splited[3]} \n ${splited[4]} \n ${splited[5]} \n ${splited[6]} \n ${splited[7]}`)
